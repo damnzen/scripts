09e78874cdceba0e43aaa39371c1dc5a3485e9e1
damnzen <damnzen@users.noreply.github.com>
2018-01-16 12:21:00 +0000

    Update and rename amazon_autocomp.js to amazon.js

--- data-sources/amazon.js
+++ data-sources/amazon.js
@@ -10,19 +10,43 @@ var discogs = new Discogs("Consumer key" ,"Consumer secret" , "release" );
 var r = discogs.search(query);
 result( r , function(id) { return discogs.extra(id);});
 */
-function AmazonComp () {
+function Amazon () {
 }
 
 /**
 Issue a search query to Discogs database.
 @param {string} query - Search query.
 */
-AmazonComp.prototype.search = function(query) {
+Amazon.prototype.autocomp = function(query) {
 	var result = http().get("http://completion.amazon.co.jp/search/complete?mkt=6&method=completion&search-alias=aps&q=" + encodeURIComponent(query));
 	var json = JSON.parse(result.body);
 	var resultArray = [];
 	for each(var i in json[1]){
 		resultArray.push({"title": i});
+	}
+	return resultArray
+}
+
+Amazon.prototype.amazlet = function(query) {
+	var result = http().get("http://app.bloghackers.net/amazlet/?type=all&__mode=keywordsearch&locale=jp&keyword=" + encodeURIComponent(query));
+	var items = r.body.split('<div class="search-result">');
+	delete items[0];
+	delete items[items.length-1];
+
+	var resultArray = [];
+	for each(item in items){
+		if (/<p class="title"><a href="(.*?)" target="_blank">(.*?)<\/a>/.test(item)){
+			var o = {};
+			o["id"] = o;
+			o["title"] = RegExp.$2;
+			o["url"] = RegExp.$1;
+			if (/<span class="result-price">(.*?)<\/span>/.test(item)) o["desc"] = RegExp.$1;
+			if (/<img src="(.*?)"/.test(item)){
+				o["thumb"] = RegExp.$1;
+				o["image"] = o["thumb"].replace("SL75_", "SL1500_");
+			}
+			resultArray.push(o);
+		}
 	}
 	return resultArray
 }
