ea5dd89931750656b6dad5c0b026f9a749073b92
damnzen <damnzen+github@gmail.com>
2019-09-16 01:56:26 +0000

    a

--- data-sources/gnavi.js
+++ data-sources/gnavi.js
@@ -11,52 +11,11 @@ var r = discogs.search(query);
 result( r , function(id) { return discogs.extra(id);});
 */
 
-function isHiragana(str){
-  str = (str==null)?"":str;
-  if(str.match(/^[ぁ-んー　]*$/)){    //"ー"の後ろの文字は全角スペースです。
-    return true;
-  }else{
-    return false;
-  }
-}
-
-function hiraToKata(str){
-    return str.replace(/[ぁ-ん]/g, function(s) {
-        return String.fromCharCode(s.charCodeAt(0) + 0x60);
-    });
-}
-
-function kataToHira(str){
-    if (typeof str == "undefined") return "";
-    return str.replace(/[ァ-ン]/g, function(s) {
-        return String.fromCharCode(s.charCodeAt(0) - 0x60);
-    });
-}
-
-function flatten(o, key){
-    var sub = o[key];
-    if(typeof sub == "undefined") return;
-    Object.keys(sub).forEach(function(subkey){
-        if(Array.isArray(sub[subkey])){
-            o[subkey] = sub[subkey].join(',');
-        }else if(typeof sub[subkey] == "object"){
-            o[subkey] = flatten(sub, subkey);
-        }else{
-            o[subkey] = sub[subkey]
-        }
-
-    });
-}
-
 function Gnavi(apiKey) {
     this.apiKey = apiKey;
 }
 
 
-/**
-Issue a search query to Discogs database.
-@param {string} query - Search query.
-*/
 Gnavi.prototype.search = function(query) {
     if(isHiragana(query)){
         var result = http().get("https://api.gnavi.co.jp/RestSearchAPI/v3/?name_kana=" + encodeURIComponent(hiraToKata(query)) + "&keyid=" + this.apiKey);
