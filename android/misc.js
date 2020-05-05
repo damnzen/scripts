69d49af983dc24ad229ef263353e15364a269806
damnzen <damnzen@users.noreply.github.com>
2020-05-05 20:52:55 +0900

    Update misc.js

--- android/misc.js
+++ android/misc.js
@@ -1,1 +1,12 @@
+var MyUtil = {
+    atc : function (filter, cmd, pars){
+   var url = "autotoolscommand://" + [filter, cmd].concat(pars).join("=:=");
 
+   i = intent("android.intent.action.VIEW");
+   i.data(url);
+   i.send();
+    },
+  
+  
+
+}
