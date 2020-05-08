eaf4ab9462fe704ebbbcea7ccd03ea9c0e45ea02
damnzen <damnzen@users.noreply.github.com>
2020-05-08 15:33:26 +0900

    Add files via upload

---
+++ data-sources/openbd.js
@@ -0,0 +1,34 @@
+function OpenBD() {
+}
+
+OpenBD.prototype.convertDate = function(datestr) {
+	 var d = new Date(datestr.substr(0,4), datestr.substr(4,2),datestr.substr(6,2));
+	return d.getTime();
+}
+
+OpenBD.prototype.lookup = function(isbn) {
+	var url = "https://api.openbd.jp/v1/get?isbn=" + isbn + "&pretty";
+	var req = http();
+	var res = req.get(url);
+	var json = JSON.parse(res.body);
+
+	var o = json[0]["onix"];
+	o["title"] = o["DescriptiveDetail"]["TitleDetail"]["TitleElement"]["TitleText"]["content"];
+	o["Author"] = o["DescriptiveDetail"]["Contributor"].map(e => e["PersonName"]["content"]).join(", ");
+	o["Publisher"] = o["PublishingDetail"]["Imprint"].ImprintName;
+	o["PublishedDate"] = this.convertDate(o["PublishingDetail"].PublishingDate[0]["Date"]);
+	o["pageCount"] = o["DescriptiveDetail"].Extent[0].ExtentValue;
+	o["description"] = o["CollateralDetail"].TextContent[0].Text;
+	o["isbn"] = o.RecordReference;
+	if("SupportingResource" in o["CollateralDetail"]) o["image"] = o["CollateralDetail"].SupportingResource[0].ResourceVersion[0].ResourceLink;
+	if("Subject" in o["DescriptiveDetail"]){
+		var ccode = o["DescriptiveDetail"].Subject[0].SubjectCode;
+		var r = BookUtil.convertCcode(ccode);
+		Object.assign(o,r);
+	}
+	return o;
+}
+
+var openbd = new OpenBD();
+var r = openbd.lookup(query);
+result(r);
