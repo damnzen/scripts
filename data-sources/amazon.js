0dee65fb26102e9c1bdb1bf6626c1402e7b770b4
damnzen <damnzen@users.noreply.github.com>
2020-12-12 14:23:24 +0900

    Update amazon.js

--- data-sources/amazon.js
+++ data-sources/amazon.js
@@ -5,7 +5,7 @@ function decNumRefToString(decNumRef) {
 }
 
 function cleanTitle(title){
-	return title.replace(/【.*?】/g,"")
+	return title.replace(/【.*?】|\[.*?\]|〔 .*?〕/g,"")
 }
 
 function getProductCode(title){
@@ -39,9 +39,9 @@ Amazon.prototype.autocomp = function(query) {
 	var result = req.get("http://completion.amazon.co.jp/search/complete?mkt=6&method=completion&search-alias=aps&q=" + encodeURIComponent(query));
 	var json = JSON.parse(result.body);
 	var resultArray = [];
-	for each(var i in json[1]){
+  json[1].forEach(i =>{
 		resultArray.push({"title": i});
-	}
+	});
 	return resultArray
 }
 
@@ -49,7 +49,7 @@ Amazon.prototype.search = function(query){
 	var url = "https://www.amazon.co.jp/s?k=" + query;
 	var req = http();
 	//http.headers({"User-Agent": "Mozilla/5.0 (iPhone; CPU OS 10_14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.1.1 Mobile/14E304 Safari/605.1.15"});
-	req.headers({"User-Agent": "D501i DoCoMo/1.0/D501"});
+	//req.headers({"User-Agent": "D501i DoCoMo/1.0/D501"});
 req.headers({"User-Agent": "Mozilla/5.0 (Linux; Android 9.0; Z832 Build/MMB29M) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Mobile Safari/537.36"});
 //req.headers({"User-Agent": "Opera/9.80 (Android; Opera Mini/8.0.1807/36.1609; U; en) Presto/2.12.423 Version/12.16"});
 	//req.headers({"User-Agent": ("D501i DoCoMo/1.0/D5" + new Date().getSeconds()).toString()});
@@ -64,16 +64,22 @@ req.headers({"User-Agent": "Mozilla/5.0 (Linux; Android 9.0; Z832 Build/MMB29M) 
 		//log(div);
 		if(!/^="([A-Z0-9]+)/.test(div)) return;
 		var asin = RegExp.$1;
+  if(asin == "") return
 		var m = div.match(regexp);
 		if (!m) return;
+		var title = cleanTitle(m[2]);
+		var image = imageFromThumb(m[1])
+		if (title.indexOf("スポンサー")==0) return;
+		if (image.indexOf("//")==0) return;
 		//log(/<img src="(.*)"[\s\S]*?alt="(.*)"/.exec(div));
 		o = {
 			"id" : asin,
 			"asin" : asin,
-			"title" : cleanTitle(m[2]),
-			"desc" : getProductCode(m[2]),
+			"title" : title,
+//			"desc" : getProductCode(m[2])　+ " " + title,
+			"desc" : title,
 			"thumb" : m[1],
-			"image" : imageFromThumb(m[1]),
+			"image" : image,
 			"amazonUrl" : "https://www.amazon.co.jp/o/ASIN/" + asin + "/",
 //			"image" : "http://images-jp.amazon.com/images/P/" + asin + ".09.LZZZZZZZ.jpg",
 		};
@@ -85,13 +91,11 @@ req.headers({"User-Agent": "Mozilla/5.0 (Linux; Android 9.0; Z832 Build/MMB29M) 
 }
 
 /*
-
 Amazon.prototype.amazlet = function(query) {
 	var result = http().get("http://app.bloghackers.net/amazlet/?type=all&__mode=keywordsearch&locale=jp&keyword=" + encodeURIComponent(query));
 	var items = result.body.split('<div class="search-result">');
 	delete items[0];
 //	delete items[items.length-1];
-
 	var resultArray = [];
 	for each(item in items){
 		if (/<p class="title"><a href="(.*?)" target="_blank">(.*?)<\/a>/.test(item)){
@@ -113,7 +117,6 @@ Amazon.prototype.amazlet = function(query) {
 	}
 	return resultArray
 }
-
 */
 
 Amazon.prototype.rss = function(query) {
@@ -129,7 +132,7 @@ var json = dom.toJSON();
 var items = json["feed"]["entry"];
 //log(dom);
 var resultList = [];
-for each(var item in items){
+items.forEach(item => {
 	
 //    "title" : item.getElementsByTagName("title")[0].innerText,
 //    "desc" : item.getElementsByTagName("summary")[0].innerText,
@@ -139,7 +142,7 @@ for each(var item in items){
 //  o["thumb"] = "http://images-jp.amazon.com/images/P/" + asin + ".09.THUMBZZZ.jpg";
 //  o["id"] = o;
   resultList.push(item);
-}
+});
 return resultList;
 
 }
