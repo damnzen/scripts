ceb7c320a4b637e1e70274490dd01e0c9cc09253
damnzen <damnzen@users.noreply.github.com>
2021-02-21 13:00:37 +0900

    Update kakaku.js

--- data-sources/kakaku.js
+++ data-sources/kakaku.js
@@ -1,8 +1,3 @@
-/*
-function jsonUrl(url) {
-	return 'https://script.google.com/macros/s/AKfycbxRjzQftnEWO7fwETplJpsJMC79UTlXCQGSYjmdac3vHGJYFida/exec?url=' + encodeURIComponent(url);
-}
-*/
 function getProductCode(title){
 	var m = title.match(/(?=.*[0-9])[\-A-Z0-9]{6,}/g);
 	if (m){
@@ -49,7 +44,7 @@ Kakaku.prototype.extra = function (id) {
  
   Object.assign(json, json.product);
   json["title"] = json.productName;
-  json["image"] = json.img.mainImg.url.view;
+  json["image"] = json.img ? json.img.mainImg.url.view : null;
   json["kakakuUrl"] = json.shareMessages.url;
   json["productCode"] = getProductCode(json.product.productName);
   //if (json.salesDate) json["salesDate"] = Date.parse(json.salesDate.replace(/[年月日]/g, "/"));
@@ -66,64 +61,3 @@ Kakaku.prototype.shops = function (id, order, area) {
   var shops = JSON.parse(result.body);
   return shops;
 }
-/*
-Kakaku.prototype.search = function (query) {
-  var url = 'http://api.kakaku.com/WebAPI/ItemSearch/Ver1.0/ItemSearch.aspx?ApiKey=' + this.apiKey + '&Keyword=' + encodeURIComponent(query) + '&CategoryGroup=ALL&HitNum=20';
-  var result = http().get(jsonUrl(url));
-  var json = JSON.parse(result.body);
-  //log(result.body);
-  //log(json.query.results.ProductInfo);
-  var results = json["ProductInfo"];
-  var items;
-  if (!results){
-    items = [];
-  }else if (results.NumOfResult == 1){
-    items = [results.Item];
-  }else{
-    items = results.Item;
-  }
-  items.forEach(item => {
-    //log(item);
-    item['title'] = item.ProductName;
-    item['desc'] = [
-      item.MakerName,
-      item.CategoryName
-    ].join(' ');
-    
-    item['thumb'] = item.ImageUrl;
-    item['image'] = item.ImageUrl.replace("/m/", "/fullscale/");
-    //item['id'] = item.ProductID;
-    item['id'] = item;
-  });
-  return items;
-}
-
-Kakaku.prototype.extra = function (item) {
-  item["shops"] = Kakaku.prototype.getShops(item.ProductID);
-  item["id"] = null;
-  return item
-}
-
-Kakaku.prototype.getShops = function (productID) {
-  var result = http().get('http://kakaku.com/item/' + productID + '/');
-  //var body = ECL.charset.convert(result.body, "Unicode", "SJIS");
-  var body = result.body;
-  var matches = body.match(/<div class="p-PTShop_btn">\s\s<a onclick="cmc\(.*>/g);
-  var shops = [];
-  if (matches){
-    matches.forEach(match => {
-      if (/shpbid:(\d+),shpkey:(\d+),shpname:'(.*?)'/.test(match)) {
-        shop = {};
-        shop['price'] = RegExp.$1;
-        shop['id'] = RegExp.$2;
-        shop['name'] = RegExp.$3;
-      }
-      if (/Url=(.*?)&/.test(match)) {
-        shop['url'] = decodeURIComponent(RegExp.$1);
-      }
-      shops.push(shop);
-    });
-  }
-  return shops
-}
-*/
