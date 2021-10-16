a0e2a87b1c80d13cf45bddb60db63b48ff52bfb4
damnzen <damnzen@users.noreply.github.com>
2021-10-16 15:23:31 +0900

    Update filmarks.js

--- data-sources/filmarks.js
+++ data-sources/filmarks.js
@@ -4,29 +4,38 @@ function Filmarks(){
 
 Filmarks.prototype.autocompleteForId= function(query, limit){
   limit = limit || 10;
-  var rs = this.autocompleteMovie(query, limit);
-  var rs2 = this.autocompleteTv(query, limit);
-  
-  var result = rs.map(r => ({
-         title : "movies/" + r["id"],
-         thumb : r["imagePath"],
-         desc : r["title"],
-                   id : "movies/" + r["id"],
-                   })).concat(rs2.map(r => ({
-         title : "drama/seasons/" + r["id"],
-         //title : "dramas/" + r["seriesId"] + "/" + r["id"],
-         thumb : r["imagePath"],
-         desc : r["title"],
-                   id : "drama/seasons/" + r["id"],
-                   })));
+  var title = query[0]=="/" ? query.substring(3) : query;
+  var rs1 = [], rs2 = [];
+  if(query.slice(0,3) == "/m:" || query[0] != "/"){
+    rs1 = this.autocompleteMovie(query, limit);
+    rs1 = rs1.map(r => ({
+          title : "movies/" + r["id"],
+          thumb : r["imagePath"],
+          desc : r["title"],
+                    id : "movies/" + r["id"],
+                    }));
+  }
+  if(query.slice(0,3) == "/t:" || query[0] != "/"){
+    rs2 = this.autocompleteTv(query, limit);
+    rs2 = rs2.map(r => ({
+          title : "drama/seasons/" + r["id"],
+          //title : "dramas/" + r["seriesId"] + "/" + r["id"],
+          thumb : r["imagePath"],
+          desc : r["title"],
+                    id : "drama/seasons/" + r["id"],
+                    }));
+  }
+  var result = rs1.concat(rs2);
   return result
 }
 
 Filmarks.prototype.autocomplete = function(query, limit){
+  limit = limit || 10;
   return this.autocompleteMovie(query, limit);
 }
 
 Filmarks.prototype.autocompleteMovie = function(query, limit){
+  limit = limit || 10;
   var url = "https://api.filmarks.com/v2/movies?autocomplete=1&limit=" + limit + "&q=" + encodeURIComponent(query);
   var req = http();
   var res = req.get(url);
@@ -37,6 +46,7 @@ Filmarks.prototype.autocompleteMovie = function(query, limit){
 }
 
 Filmarks.prototype.autocompleteTv = function(query, limit){
+  limit = limit || 10;
   var url = "https://api.dramarks.com/api/v2/drama/seasons?autocomplete=1&limit=" + limit + "&q=" + encodeURIComponent(query);
   var req = http();
   var res = req.get(url);
@@ -92,3 +102,4 @@ Filmarks.prototype.lookup = function(id, limit){
   
 return r
 }
+
